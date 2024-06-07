f4217274f0307c2e5aaa40ad4937e593f4dd9f4a
Aileen Nguyen <aileenn@gmail.com>
2024-06-06 17:42:52 -0700

    created css file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 import Footer from './components/Footer';
+import "./components/style.css";
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
