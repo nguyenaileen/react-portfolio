587c3f5efb8393d80ce904b4bf0d0de6ca1e67f6
Aileen Nguyen <aileenn@gmail.com>
2024-06-06 21:31:25 -0700

    edits to file structure for deplpyment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./app.css";
+import "./style.css";
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
